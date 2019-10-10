f20f8bb15df3e8ce08de3e08f5002f76860ae434
Ruxin Zhang <35670612+ruxin827@users.noreply.github.com>
2019-10-09 22:04:30 -0700

    Update HelloControllerIT.java

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -38,7 +38,7 @@ public class HelloControllerIT {
                 String.class);
         String rightAnswer = "<h1>Hello World!</h1>\n" +
                         "<p>This web app is powered by \n" +
-                        "<a href='https://github.com/mygithubid/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+                        "<a href='https://github.com/ruxin827/spring-boot-minimal-webapp'>this github repo</a></p>\n";
         assertThat(response.getBody(), equalTo(rightAnswer));
     }
 }
